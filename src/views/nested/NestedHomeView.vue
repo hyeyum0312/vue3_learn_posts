5d29166fd297635cf9b0774c6c8028fe86c22764
조혜연 <hyeyum0312@gmail.com>
2024-01-29 15:28:01 +0900

    feat[vue3]: router push와 replace의 차이

--- src/views/nested/NestedHomeView.vue
+++ src/views/nested/NestedHomeView.vue
@@ -10,9 +10,3 @@
 <script setup></script>
 
 <style lang="scss" scoped></style>
-
-<!-- 
-  route.replace
-  route.push와 같은 역할을 하지만 유일한 차이는 새로운 히스토리 항목에 추가하지 않고 탐색한다는 것 이다.
-  이름에서 알 수 있든이 현재 항목을 대체한다.
--->
--- src/views/nested/NestedView.vue
+++ src/views/nested/NestedView.vue
@@ -2,12 +2,34 @@
   <div class="container py-4">
     <ul class="nav nav-pills">
       <li class="nav-item">
-        <RouterLink to="/nested/one" class="nav-link" active-class="active">
+        <!-- history가 남는다. -->
+        <!-- <RouterLink to="/nested/one" class="nav-link" active-class="active">
+          Nested1
+        </RouterLink> -->
+        <!-- <RouterLink to="/nested/two" class="nav-link" active-class="active">
+          Nested2
+        </RouterLink> -->
+
+        <!-- 
+            route.replace
+            history가 남지 않는다.
+            route.push와 같은 역할을 하지만 유일한 차이는 새로운 히스토리 항목에 추가하지 않고 탐색한다는 것 이다.
+            이름에서 알 수 있든이 현재 항목을 대체한다.
+            -->
+        <RouterLink
+          :to="{ name: 'NestedOne', replace: true }"
+          class="nav-link"
+          active-class="active"
+        >
           Nested1
         </RouterLink>
       </li>
       <li class="nav-item">
-        <RouterLink to="/nested/two" class="nav-link" active-class="active">
+        <RouterLink
+          :to="{ name: 'NestedTwo', replace: true }"
+          class="nav-link"
+          active-class="active"
+        >
           Nested2
         </RouterLink>
       </li>
