e39262f0d2b4b8c439f284d10e883cb6bc4a158b
조혜연 <hyeyum0312@gmail.com>
2024-02-22 23:49:00 +0900

    feat: 공통컴포넌트

--- src/posts/PostCreateView.vue
+++ src/posts/PostCreateView.vue
@@ -2,48 +2,35 @@
   <div>
     <h2>게시글 등록</h2>
     <hr class="my-4" />
-    <form @submit.prevent="save">
-      <div class="mb-3">
-        <label for="title" class="form-label"> 제목 </label>
-        <input
-          v-model="form.title"
-          type="text"
-          class="form-control"
-          id="title"
-        />
-      </div>
+    <PostForm
+      v-model:title="form.title"
+      v-model:content="form.content"
+      @submit.prevent="edit"
+    >
+      <template #actions>
+        <div class="pt-4">
+          <button
+            type="button"
+            class="btn btn-outline-dark me-2"
+            @click="goListPage"
+          >
+            목록
+          </button>
 
-      <div class="mb-3">
-        <label for="contents" class="form-label"> 내용 </label>
-        <textarea
-          v-model="form.content"
-          class="form-control"
-          id="contents"
-          rows="3"
-        ></textarea>
-      </div>
-
-      <div class="pt-4">
-        <button
-          type="button"
-          class="btn btn-outline-dark me-2"
-          @click="goListPage"
-        >
-          목록
-        </button>
-
-        <button type="button" class="btn btn-outline-primary" @click="save">
-          저장
-        </button>
-      </div>
-    </form>
+          <button type="button" class="btn btn-outline-primary" @click="save">
+            저장
+          </button>
+        </div>
+      </template>
+    </PostForm>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { createPost } from "@/api/posts";
+import PostForm from "@/components/posts/PostForm.vue";
 
 const router = useRouter();
 
